e75317542dbe4ab708068c1a0cda0b57a5b7cd7f
Piotr Iwaniuk <piwaniuk@esri.pl>
2017-11-09 15:24:20 +0100

    feat: middleware added

--- server-static.js
+++ server-static.js
@@ -4,16 +4,29 @@ const app = express();
 
 app.use(express.static('assets'));
 
+app.use((req, res, next) => {
+  res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz');
+});
+
+app.use('/store', (req, res, next) => {
+  console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+  next();
+});
+
+// WHY middleware is not displayed if no callback set?
 app.get('/', (req, res) => (
   res.sendFile('/index.html')
 ));
-
 
 app.get('/userform', (req, res) => {
   const { first_name, last_name } = req.query;
   const response = { first_name, last_name }; // oneliner?
 
   res.end(JSON.stringify(response));
+});
+
+app.get('/store', (req, res) => {
+  res.send('To jest sklep');
 });
 
 const server = app.listen(3000, 'localhost', () => {
