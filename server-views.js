1dbacd1eb28f1ad3559b4d58d232892136e2f8c1
piotr <pyhivo@gmail.com>
2018-01-25 08:45:05 +0100

    temp

--- server-views.js
+++ server-views.js
@@ -2,25 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('style'));
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use('/store', (req, res, next) => {
-  console.log('Jestem pośrednikiem przy żądaniu do /store');
-  next();
-});
-
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('content');
 });
 
-app.get('/store', (req, res) => {
-  res.send('To jest sklep');
-});
-
-app.get('/first-template', (req, res) => {
-  res.render('first-template');
-});
 
 app.listen(3000);
 app.use((req, res, next) => {
